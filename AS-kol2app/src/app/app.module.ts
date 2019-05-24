7b0d1df421cfefb72d8c64d9b5f2d37c37ae915b
adastr96 <adam19960@op.pl>
2019-05-24 17:57:04 +0200

    kolokwium2

--- AS-kol2app/src/app/app.module.ts
+++ AS-kol2app/src/app/app.module.ts
@@ -4,10 +4,10 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { BlogItemComponent } from './components/blog-item/blog-item.component';
-import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
-import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
+// import { BlogComponent } from './components/blog/blog.component';
+// import { BlogItemComponent } from './components/blog-item/blog-item.component';
+// import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
+// import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
 import { DataService } from './service/data.service';
 import { ASDataService } from './service/as-data.service';
 import { ASBlogComponent } from './components/as-blog/as-blog.component';
@@ -17,10 +17,10 @@ import { ASBlogItemDetailsComponent } from './components/as-blog-item-details/as
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent,
-    BlogItemComponent,
-    BlogItemTextComponent,
-    BlogItemImageComponent,
+    // BlogComponent,
+    // BlogItemComponent,
+    // BlogItemTextComponent,
+    // BlogItemImageComponent,
     ASBlogComponent,
     ASBlogItemComponent,
     ASBlogItemDetailsComponent
